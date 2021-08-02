fd438a3a3c33bc9127f16e2949e1c746ad76d826
EspiraMarvin <espiramarvin@gmail.com>
2021-08-02 23:52:55 +0300

    feat: added canvas for sidebar

---
+++ components/layouts/Canvas.js
@@ -0,0 +1,34 @@
+import {Button, Offcanvas, ListGroup} from "react-bootstrap";
+import React, {useState} from "react";
+import Link from "next/link";
+const Canvas = () => {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Add
+            </Button>
+
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>DaoList</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <ListGroup as="ul">
+                        <Link href="/new">
+                            <ListGroup.Item as="li" active>
+                            Add New Dao
+                        </ListGroup.Item>
+                        </Link>
+                    </ListGroup>
+                </Offcanvas.Body>
+            </Offcanvas>
+            </>
+    )
+
+}
+export default Canvas
--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,10 +1,10 @@
 import Footer from "../Footer";
-import Navbar from "../Navbar";
+import Header from "../Header";
 
 const Layout = ({ children }) => {
     return (
         <div className="content">
-            <Navbar />
+            <Header />
             { children }
             <Footer />
         </div>
